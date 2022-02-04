52fe7ac2b27bf065c96a2b213342f65d3bf82fca
empdo <emil.essung@live.se>
2022-02-04 16:22:26 +0100

    add text

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,6 +4,7 @@ export default () => {
     
     const cards = [ "I have work experince in both stores and web/app development", "I study information technology at a upper secondary level", "I am a 18 year old developer from sweden"];
     const [offset, setOffset] = React.useState(0);
+
     
     
     const stuff = cards.map((content,layer) => {
@@ -11,7 +12,8 @@ export default () => {
 
         return (
             <div onClick={() => setOffset(offset + 1)} key={content} className="card" style={{zIndex: realIndex, marginRight: realIndex*20, marginTop: realIndex*20, opacity: ((realIndex +1) / (cards.length) ) }} >
-                <p>{content}</p>
+                 <p>{content}</p>
+                {offset ===  0 ? <p id="click-alert">click me!</p> : <></>}
             </div>
         )
     });
